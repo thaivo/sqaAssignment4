2e0ce2b48709aab190650f6596c68b07800282da
thaivo <thai2903@gmail.com>
2021-08-10 09:12:47 -0400

    Added dist folder

---
+++ dist/assets/index.3575d89c.js
@@ -0,0 +1,1 @@
+var e={headline:"HTTP5201-Assignment 4 Home page!",href:"index.html"};const n=document.getElementById("app"),a='<a href="'+e.href+'">'+e.headline+"</a>";var h;h=a,n.innerHTML=`<h1>${h}</h1>`;
